cc348a10f16dd0dcca506c466260fbe0b16120ca
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-15 18:01:15 +0200

    update script created methods and return data from axios call

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,39 @@
 </template>
 
 <script>
-import {store} from './store.js';
-import axios from 'axios';
-import HeaderComponent from './components/HeaderComponent.vue';
-import MainComponent from './components/MainComponent.vue';
-  export default {
-    name: 'App',
-    components:{
-      HeaderComponent,
-      MainComponent,
+import { store } from "./store.js";
+import axios from "axios";
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+    MainComponent,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getCards() {
+      axios
+        .get(this.store.apiPath + this.store.endPoint.cardSearch)
+        .then((res) => {
+          this.store.Cards = res.data.data;
+          console.log(this.store.Cards);
+        }).catch((error) => {
+          console.log(error);
+        }).finally(() => {
+          
+        });
     },
-    data() {
-      return {
-        store,
-      }
-    },
-    created(){
-      axios.get(this.store.apiPath + this.store.endPoint.cardSearch).then((res) => {
-        this.store.Cards = res.data.data
-        console.log(this.store.Cards);
-      })
-    }
-  }
+  },
+  created() {
+    this.getCards();
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,5 +1,4 @@
 <template>
-    
 <CardComponent />
 </template>
 
