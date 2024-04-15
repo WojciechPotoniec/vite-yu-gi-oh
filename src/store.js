7eed3b86524b777e582825d555f990d97e407d0e
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-15 17:36:30 +0200

    created data file

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiPath: "https://db.ygoprodeck.com/api/v7/",
+    endPoint: {
+        cardSearch: "cardinfo.php?num=20&offset=0"
+    }
+});
